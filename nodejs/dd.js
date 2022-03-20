0b78dc18030d04391797447b2c20b1bd1a9c59ba
Judongjae <wnehdwo0982@naver.com>
2022-03-21 04:29:53 +0900

    백준

--- nodejs/dd.js
+++ nodejs/dd.js
@@ -1,14 +1,109 @@
-let numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5];
-let hand = right;
-let result = [];
-function solution(numbers, hand) {
-  for (i = 0; i < numbers.length; i++) {
-    if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
-      result[i] = [L];
-    } else if (numbers[i] == 3 || numbers[i] == 6 || numbers[9]) {
-      result[i] = [R];
+const fs = require("fs");
+
+const readFileSyncAddress = "input.txt";
+
+const input = fs
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, ...commands] = input;
+
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  push(item) {
+    const node = new Node(item);
+    if (!this.head) {
+      this.head = node;
+      this.head.next = this.tail;
+    } else {
+      this.tail.next = node;
+    }
+
+    this.tail = node;
+    this.size += 1;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  pop() {
+    if (this.size > 2) {
+      const item = this.head.item;
+      const newHead = this.jead.next;
+      this.head = newHead;
+      this.size -= 1;
+      return item;
+    } else if (this.size === 2) {
+      const item = this.head.item;
+      const newHead = this.head.next;
+      this.head = newHead;
+      this.tail = newHead;
+      this.size -= 1;
+      return item;
+    } else if (this.size === 1) {
+      const item = this.head.item;
+      this.head = null;
+      this.tail = null;
+      this.size -= 1;
+      return item;
+    } else {
+      return -1;
     }
   }
-  var answer = "";
-  return answer;
+  empty() {
+    return this.size ? 0 : 1;
+  }
+  front() {
+    return this.head ? this.head.item : -1;
+  }
+  back() {
+    return this.tail ? this.tail.item : -1;
+  }
 }
+function solution(n, commands) {
+  let answer = "";
+  const queue = new Queue();
+
+  for (let i = 0; i < n; i += 1) {
+    const command = commands[i].split(" ")[0];
+
+    switch (command) {
+      case "push":
+        const pushItem = commands[i].split(" ")[1];
+        queue.push(pushItem);
+        break;
+      case "pop":
+        answer += queue.pop() + " ";
+        break;
+      case "front":
+        answer += queue.back() + " ";
+        break;
+      case "empty":
+        answer += queue.empty() + " ";
+        break;
+      case "size":
+        answer += queue.getSize() + " ";
+        break;
+      default:
+        break;
+    }
+  }
+  return answer.split(" ").join("\n");
+}
+
+const answer = solution(n, commands);
+console.log(answer);
