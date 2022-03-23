5461b5e78925c45aa7d66b2b64887ff533a0db5d
Judongjae <wnehdwo0982@naver.com>
2022-03-23 21:45:04 +0900

    백준문제

--- nodejs/dd.js
+++ nodejs/dd.js
@@ -1,68 +1,25 @@
-class minHeap {
-  constructor() {
-    this.heap = [];
-    this.heap.push(-Infinity);
-  }
-  insert(val) {
-    this.heap.push(val);
-    this.upheap(this.heap.length - 1);
-  }
-  upheap(pos) {
-    let tmp = this.heap[pos];
-    while (tmp < this.heap[parseInt(pos / 2)]) {
-      this.heap[pos] = this.heap[parseInt(pos / 2)];
-      pos = parseInt(pos / 2);
-    }
-    this.heap[pos] = tmp;
-  }
-  get() {
-    if (this.heap.length === 2) return this.heap.pop();
-    let res = this.heap[1];
-    this.heap[1] = this.heap.pop();
-    this.downheap(1, this.heap.length - 1);
-    return res;
-  }
-  downheap(pos, len) {
-    let tmp = this.heap[pos],
-      child;
-    while (pos <= parseInt(len / 2)) {
-      child = pos * 2;
-      if (child < len && this.heap[child] > this.heap[child + 1]) child++;
-      if (tmp <= this.heap[child]) break;
-      this.heap[pos] = this.heap[child];
-      pos = child;
-    }
-    this.heap[pos] = tmp;
-  }
-  size() {
-    return this.heap.length - 1;
-  }
-  front() {
-    return this.heap[1];
-  }
+using namespace std;
+int dp[100001];
+
+int main() {
+
+	int N;
+	scanf("%d", &N);
+
+	dp[0] = 0; dp[1] = 1;
+
+	for (int i = 0; i <= N; i++)
+		dp[i] = i;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j * j <= i; j++) {
+
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+			// 이후 j의 반복이 계속되어, 기존 dp[i]의 값과 새로운 dp[i - j * j] + 1
+			// 과의 비교를 하려면 min 의 인자에 들어갈 두 값들은 위와 같다.
+		}
+	}
+
+	printf("%d",dp[N]);
+
 }
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
-let n = Number(input[0]); // 연산의 개수
-let index = 1;
-let minQueue = new minHeap();
-let result = "";
-while (index <= n) {
-  if (Number(input[index]) === 0) {
-    // 가장 작은 값 출력하고 제거
-    if (minQueue.size() === 0) {
-      // console.log(0);
-      result += `0\n`;
-    } else {
-      //   console.log(minQueue.front());
-      result += `${minQueue.front()}\n`;
-      minQueue.get();
-    }
-  } else {
-    // input[index]값 배열에 넣기
-    minQueue.insert(Number(input[index]));
-  }
-  index++;
-}
-console.log(result.trim());
