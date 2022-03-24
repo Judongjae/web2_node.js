0bdb798ac3a7ff5b2d9962d08dd5931a406322d0
Judongjae <wnehdwo0982@naver.com>
2022-03-24 23:46:10 +0900

    프로그래머스 1단계

--- nodejs/dd.js
+++ nodejs/dd.js
@@ -1,25 +1,8 @@
-using namespace std;
-int dp[100001];
+function solution(nums) {
+  var answer = -1;
 
-int main() {
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  console.log("Hello Javascript");
 
-	int N;
-	scanf("%d", &N);
-
-	dp[0] = 0; dp[1] = 1;
-
-	for (int i = 0; i <= N; i++)
-		dp[i] = i;
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j * j <= i; j++) {
-
-			dp[i] = min(dp[i], dp[i - j * j] + 1);
-			// 이후 j의 반복이 계속되어, 기존 dp[i]의 값과 새로운 dp[i - j * j] + 1
-			// 과의 비교를 하려면 min 의 인자에 들어갈 두 값들은 위와 같다.
-		}
-	}
-
-	printf("%d",dp[N]);
-
+  return answer;
 }
